809fb577b74ac18e3d64498b253a55c2519ef221
takuyaK <welcom4869@icloud.com>
2021-07-08 18:44:06 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -7,10 +7,30 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];//argvにはアドレスが格納されている https://dev.grapecity.co.jp/support/powernews/column/clang/028/page02.htm
+
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));//pというポインタの値が格納されている住所を&pは表している。そしてstrtolはその住所に新たな値を代入している
+
+  while (*p) {//*pはpというポインタが表す住所に入っている値を表す。https://www.sgnet.co.jp/technology/c/6-3/
+    if (*p == '+') {
+      p++;//これはポインタが表す番地に1を足している
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
